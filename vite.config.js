5f3c9ab74273d3bb73078277da7be5e96f7ec7c4
nayani <yapancs@gmail.com>
2025-02-08 02:08:56 -0500

    Initial commit

--- vite.config.js
+++ vite.config.js
@@ -7,8 +7,18 @@ export default defineConfig({
   server: {
     // Handle fallback for SPA routes
     historyApiFallback: true,
-    port: 3000
+    port: 3000,
+
+    // Proxy configuration
+    proxy: {
+      '/api': {
+        target: 'http://localhost:10000',  // local backend server URL
+        changeOrigin: true,            // Enable if backend is on a different domain
+        rewrite: (path) => path.replace(/^\/api/, ''),  // Optional: Remove `/api` prefix
+      },
+    },
   },
+
   test: {
     environment: 'jsdom', // Simulate a browser environment
     globals: true, // Enable global functions like `describe` and `it`
